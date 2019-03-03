8f3b5eecf244d34eda8bb2543d505297410a6b95
Njarasoa M. J. Rakotozafy <raknjarasoa@gmail.com>
2019-03-03 15:48:28 +0100

    Setup prisma graphql

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'preserve'
+};
