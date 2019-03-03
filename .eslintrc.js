64bd4184d9a9223f84d4a9cf28fea356c23500c4
Njarasoa M. J. Rakotozafy <raknjarasoa@gmail.com>
2019-03-03 15:45:59 +0100

    Config linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off'
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  }
+};
